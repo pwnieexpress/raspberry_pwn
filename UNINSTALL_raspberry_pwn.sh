b3d8f9b68951c6f94f82fc9598f084a07259d6bb
jcran <jcran@pwnieexpress.com>
2012-08-15 01:12:04 -0500

    tweak uninstall

--- UNINSTALL_raspberry_pwn.sh
+++ UNINSTALL_raspberry_pwn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Raspberry Pwn 0.1 : A Raspberry Pi Pentesting suite by Pwnie Express
+# Raspberry Pwn 0.2 : A Raspberry Pi Pentesting suite by Pwnie Express
 # pwnieexpress.com
 # UNINSTALLER Revision 6.12.2012
 
@@ -16,7 +16,7 @@ echo " | _ \ \    / / \| |_ _| __| | __\ \/ / _ \ _ \ __/ __/ __|     "
 echo " |  _/\ \/\/ /| .\` || || _|  | _| >  <|  _/   / _|\__ \__ \    "
 echo " |_|   \_/\_/ |_|\_|___|___| |___/_/\_\_| |_|_\___|___/___/     "
 echo ""
-echo "        === Raspberry Pwn Release 0.1 UNINSTALLER ===           "
+echo "        === Raspberry Pwn Release 0.2 UNINSTALLER ===           "
 echo ""
 echo "----------------------------------------------------------------"
 echo " This UNINSTALLER will remove the Raspberry Pwn pentesting      "
@@ -27,7 +27,7 @@ read INPUT
 echo ""
 
 echo "[+] Removing baseline pentesting tools/dependencies..."
-aptitude -y remove nmap dsniff netcat nikto xprobe python-scapy wireshark tcpdump ettercap hping3 medusa macchanger nbtscan john ptunnel p0f ngrep tcpflow openvpn iodine httptunnel cryptcat sipsak yersinia smbclient sslsniff tcptraceroute pbnj netdiscover netmask udptunnel dnstracer sslscan medusa ipcalc dnswalk socat onesixtyone tinyproxy dmitry fcrackzip ssldump fping ike-scan gpsd darkstat swaks arping tcpreplay sipcrack proxychains proxytunnel siege sqlmap wapiti skipfish w3af libssl-dev libpcap-dev libpcre3 libpcre3-dev libnl-dev libncurses-dev subversion python-twisted-web python-pymssql git
+apt-get -y remove nmap dsniff netcat nikto xprobe python-scapy wireshark tcpdump ettercap-text-only hping3 medusa macchanger nbtscan john ptunnel p0f ngrep tcpflow openvpn iodine httptunnel cryptcat sipsak yersinia smbclient sslsniff tcptraceroute pbnj netdiscover netmask udptunnel dnstracer sslscan medusa ipcalc dnswalk socat onesixtyone tinyproxy dmitry fcrackzip ssldump fping ike-scan gpsd darkstat swaks arping tcpreplay sipcrack proxychains proxytunnel siege sqlmap wapiti skipfish w3af libssl-dev libpcap-dev libpcre3 libpcre3-dev libnl-dev libncurses-dev subversion python-twisted-web python-pymssql git
 
 echo "[+] Removing wireless pentesting tools..."
 aptitude -y remove kismet
@@ -44,7 +44,7 @@ cp src/motd.tail.original /etc/motd.tail
 
 echo ""
 echo "---------------------------------------------------------------"
-echo "Raspberry Pwn Release 0.1 UNINSTALLED successfully!"
+echo "Raspberry Pwn Release 0.2 UNINSTALLED successfully!"
 echo "---------------------------------------------------------------"
 echo ""
 exit 1
