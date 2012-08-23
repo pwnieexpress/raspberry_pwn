bfd2099b1d4a4a93b6396ec9b8464b6e896b3195
jcran <jcran@pwnieexpress.com>
2012-08-23 16:37:47 -0500

    revert all changes attempting to make us compatible with raspbian

--- INSTALL_raspberry_pwn.sh
+++ INSTALL_raspberry_pwn.sh
@@ -1,44 +1,29 @@
 #!/bin/bash
-
-###
-### Raspberry Pwn 0.1 : A Raspberry Pi Pentesting suite by Pwnie Express
-### pwnieexpress.com
-### Installer Revision 08.14.2012
-###
+# Raspberry Pwn 0.1 : A Raspberry Pi Pentesting suite by Pwnie Express
+# pwnieexpress.com
+# Installer Revision 6.12.2012
 
 echo ""
 
 # Verify we are root
 if [[ $EUID -ne 0 ]]; then
-  echo "This script must be run as root" 1>&2
-  exit 1
+   echo "This script must be run as root" 1>&2
+   exit 1
 fi
 
 # Verify Raspberry Pwn 0.1 is not already installed
 if [ "`grep -o 0.1 /etc/motd.tail`" == "0.1" ] ; then 
-  echo "[-] Raspberry Pwn 0.1 already installed. Please install on a fresh wheezy system..."
-  exit 1
+        echo "[-] Raspberry Pwn 0.1 already installed. Aborting..."
+        exit 1
 fi
 
-# Verify Raspberry Pwn 0.2 is not already installed
-if [ "`grep -o 0.2 /etc/motd.tail`" == "0.2" ] ; then 
-  echo "[-] Raspberry Pwn 0.2 already installed. Aborting..."
-  exit 1
-fi
-
-# If we can't detect version, warn the user
-if ! [ -f /etc/motd.tail ]; then
-  echo "Unable to detect version"
-  echo "Press ENTER to continue, CTRL+C to abort."
-  read INPUT
-fi
 
 echo "  _____      ___  _ ___ ___   _____  _____ ___ ___ ___ ___      "
 echo " | _ \ \    / / \| |_ _| __| | __\ \/ / _ \ _ \ __/ __/ __|     "
-echo " |  _/\ \/\/ /| .\ || || _|  | _| >  <|  _/   / _|\__ \__ \    "
+echo " |  _/\ \/\/ /| .\` || || _|  | _| >  <|  _/   / _|\__ \__ \    "
 echo " |_|   \_/\_/ |_|\_|___|___| |___/_/\_\_| |_|_\___|___/___/     "
 echo ""
-echo "              === Raspberry Pwn Release 0.2 ===                 "
+echo "              === Raspberry Pwn Release 0.1 ===                 "
 echo "     A Raspberry Pi Pentesting suite by PwnieExpress.com        "
 echo ""
 echo "----------------------------------------------------------------"
@@ -54,21 +39,16 @@ echo ""
 # Make sure all installer files are owned by root
 chown -R root:root .
 
-# Update sources.list to include the main debian repos too
-rm /etc/apt/sources.list
-echo "deb http://archive.raspbian.org/raspbian wheezy main contrib non-free rpi" >> /etc/apt/sources.list
-echo "deb-src http://archive.raspbian.org/raspbian wheezy main contrib non-free rpi" >> /etc/apt/sources.list
-echo "deb http://ftp.debian.org/debian/ wheezy main contrib non-free" >> /etc/apt/sources.list
-
 # Update base debian packages
 echo "[+] Updating base system Debian packages..."
-apt-get -y update
-#apt-get -y upgrade
+echo "deb http://ftp.debian.org/debian/ squeeze main contrib non-free" > /etc/apt/sources.list
+aptitude -y update
+aptitude -y upgrade
 echo "[+] Base system Debian packages updated."
 
-# Install baseline pentesting tools via apt
+# Install baseline pentesting tools via aptitude
 echo "[+] Installing baseline pentesting tools/dependencies..."
-apt-get -y --force-yes install git-core build-essential perl-base python ruby irb ri rubygems libruby ruby-dev libpcap-dev telnet btscanner libnet-dns-perl hostapd nmap dsniff netcat nikto xprobe python-scapy wireshark tcpdump ettercap-text-only hping3 medusa macchanger nbtscan john ptunnel p0f ngrep tcpflow openvpn iodine httptunnel cryptcat sipsak yersinia smbclient sslsniff tcptraceroute pbnj netdiscover netmask udptunnel dnstracer sslscan medusa ipcalc dnswalk socat onesixtyone tinyproxy dmitry fcrackzip ssldump fping ike-scan gpsd darkstat swaks arping tcpreplay sipcrack proxychains proxytunnel siege sqlmap wapiti skipfish libssl-dev libpcap-dev libpcre3 libpcre3-dev libnl-dev libncurses5-dev subversion python-twisted-web python-pymssql
+aptitude -y install telnet btscanner libnet-dns-perl hostapd nmap dsniff netcat nikto xprobe python-scapy wireshark tcpdump ettercap hping3 medusa macchanger nbtscan john ptunnel p0f ngrep tcpflow openvpn iodine httptunnel cryptcat sipsak yersinia smbclient sslsniff tcptraceroute pbnj netdiscover netmask udptunnel dnstracer sslscan medusa ipcalc dnswalk socat onesixtyone tinyproxy dmitry fcrackzip ssldump fping ike-scan gpsd darkstat swaks arping tcpreplay sipcrack proxychains proxytunnel siege sqlmap wapiti skipfish w3af libssl-dev libpcap-dev libpcre3 libpcre3-dev libnl-dev libncurses-dev subversion python-twisted-web python-pymssql
 echo "[+] Baseline pentesting tools installed."
 
 # Remove unneeded statup items
@@ -77,32 +57,32 @@ update-rc.d -f gpsd remove
 update-rc.d -f tinyproxy remove
 update-rc.d -f ntp remove
 apt-get -y purge portmap
+apt-get -y autoremove gdm
 apt-get -y autoremove
 echo "[+] Unneeded startup items removed."
 
 # Install wireless pentesting tools
 echo "[+] Installing wireless pentesting tools..."
-apt-get -y --force-yes install kismet
-
-# Currently broken on Raspian
-#cd src/aircrack-ng-1.1
-#chmod +x evalrev
-#make install
-#cd ../..
-
+aptitude -y install kismet
+cd src/aircrack-ng-1.1
+chmod +x evalrev
+make install
+cd ../..
 echo "[+] Wireless pentesting tools installed."
 
 # Install Metasploit -- Note this will require changing the default RAM allocation 
 echo "[+] Installing latest Metasploit Framework..."
+aptitude -y install ruby irb ri rubygems libruby ruby-dev libpcap-dev
 mkdir /opt/metasploit
 cd /opt/metasploit
-git clone https://github.com/pwnieexpress/metasploit-framework.git msf3
+wget http://downloads.metasploit.com/data/releases/framework-latest.tar.bz2
+tar jxvf framework-latest.tar.bz2
 ln -sf /opt/metasploit/msf3/msf* /usr/local/bin/
 echo "[+] Latest Metasploit Framework installed."
 
 # Install Perl/Python tools to /pentest
 echo "[+] Installing Perl/Python tools to /pentest..."
-mv src/pentest/ /
+cp -a src/pentest/ /
 chown -R root:root /pentest/
 chmod +x /pentest/cisco-auditing-tool/CAT
 chmod +x /pentest/easy-creds/easy-creds.sh
@@ -122,22 +102,17 @@ echo "[+] SET framework installed in /pentest."
 # Update motd to show Raspberry Pwn release
 cp src/motd.tail.raspberrypwn /etc/motd.tail
 
-# Install Exploit-DB - removed to save space
-#echo "[+] Installing Exploit-DB to /pentest..."
-#svn co svn://www.exploit-db.com/exploitdb /pentest/exploitdb/
-#echo "[+] Exploit-DB installed in /pentest."
+# Install Exploit-DB
+echo "[+] Installing Exploit-DB to /pentest..."
+svn co svn://www.exploit-db.com/exploitdb /pentest/exploitdb/
+echo "[+] Exploit-DB installed in /pentest."
 
 echo "[+] Setting default RAM allocation"
 cp /boot/arm224_start.elf /boot/start.elf
 
-# Remove the debian sources from the apt sources.list - prevents problems w/ the apt package itself
-rm /etc/apt/sources.list
-echo "deb http://archive.raspbian.org/raspbian wheezy main contrib non-free rpi" >> /etc/apt/sources.list
-echo "deb-src http://archive.raspbian.org/raspbian wheezy main contrib non-free rpi" >> /etc/apt/sources.list
-
 echo ""
 echo "---------------------------------------------------------------"
-echo "Raspberry Pwn Release 0.2 installed successfully!"
+echo "Raspberry Pwn Release 0.1 installed successfully!"
 echo "---------------------------------------------------------------"
 echo ""
 
