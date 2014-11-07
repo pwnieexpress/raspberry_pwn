6476befe749b6451e60a99a1d4576fe2ae931966
root <root@raspberrypi.(none)>
2014-11-07 23:12:16 +0000

    tidyup

--- INSTALL_raspberry_pwn.sh
+++ INSTALL_raspberry_pwn.sh
@@ -49,7 +49,7 @@ echo "[+] Base system Debian packages updated."
 
 # Install baseline pentesting tools via aptitude
 echo "[+] Installing baseline pentesting tools/dependencies..."
-aptitude -y install telnet btscanner libnet-dns-perl hostapd nmap dsniff netcat nikto xprobe python-scapy wireshark tcpdump ettercap-graphical hping3 medusa macchanger nbtscan john ptunnel p0f ngrep tcpflow openvpn iodine httptunnel cryptcat sipsak yersinia smbclient sslsniff tcptraceroute pbnj netdiscover netmask udptunnel dnstracer sslscan medusa ipcalc dnswalk socat onesixtyone tinyproxy dmitry fcrackzip ssldump fping ike-scan gpsd darkstat swaks arping tcpreplay sipcrack proxychains proxytunnel siege wapiti skipfish w3af libssl-dev libpcap-dev libpcre3 libpcre3-dev libnl-dev libncurses-dev subversion python-twisted-web python-pymssql iw mc zip links w3m lynx arj dbview odt2txt gv catdvi djvulibre-bin python-boto python-tz
+aptitude -y install telnet btscanner libnet-dns-perl hostapd nmap dsniff netcat nikto xprobe python-scapy wireshark tcpdump ettercap-graphical hping3 medusa macchanger nbtscan john ptunnel p0f ngrep tcpflow openvpn iodine httptunnel cryptcat sipsak yersinia smbclient sslsniff tcptraceroute pbnj netdiscover netmask udptunnel dnstracer sslscan medusa ipcalc dnswalk socat onesixtyone tinyproxy dmitry fcrackzip ssldump fping ike-scan gpsd darkstat swaks arping tcpreplay sipcrack proxychains proxytunnel siege wapiti skipfish w3af libssl-dev libpcap-dev libpcre3 libpcre3-dev libnl-dev libncurses-dev subversion python-twisted-web python-pymssql iw mc zip links w3m lynx arj dbview odt2txt gv catdvi djvulibre-bin python-boto python-tz pkg-config
 
 echo "[+] Baseline pentesting tools installed."
 
@@ -58,8 +58,8 @@ echo "[+] Remove unneeded startup items..."
 update-rc.d -f gpsd remove
 update-rc.d -f tinyproxy remove
 update-rc.d -f ntp remove
-apt-get -y purge portmap
-apt-get -y autoremove gdm
+#apt-get -y purge portmap
+#apt-get -y autoremove gdm
 apt-get -y autoremove
 echo "[+] Unneeded startup items removed."
 
@@ -70,6 +70,7 @@ cd src/aircrack-ng-1.2-rc1
 chmod +x evalrev
 make install
 cd ../..
+airodump-ng-oui-update
 echo "[+] Wireless pentesting tools installed."
 
 # Install Metasploit -- Note this will require changing the default RAM allocation 
