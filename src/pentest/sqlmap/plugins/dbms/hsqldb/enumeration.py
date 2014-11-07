485759eb772236340fafb547c1e3d228d278dba0
ukscone <pi@raspberrypi.(none)>
2014-11-07 19:13:46 +0000

    initial fork & quick fixes so that it installs on 9-9-2014 raspbian

---
+++ src/pentest/sqlmap/plugins/dbms/hsqldb/enumeration.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2006-2014 sqlmap developers (http://sqlmap.org/)
+See the file 'doc/COPYING' for copying permission
+"""
+
+from plugins.generic.enumeration import Enumeration as GenericEnumeration
+from lib.core.data import conf
+from lib.core.data import kb
+from lib.core.data import logger
+from lib.core.data import queries
+from lib.core.common import Backend
+from lib.core.common import unArrayizeValue
+from lib.request import inject
+
+class Enumeration(GenericEnumeration):
+    def __init__(self):
+        GenericEnumeration.__init__(self)
+
+    def getBanner(self):
+        if not conf.getBanner:
+            return
+
+        if kb.data.banner is None:
+            infoMsg = "fetching banner"
+            logger.info(infoMsg)
+
+            query = queries[Backend.getIdentifiedDbms()].banner.query
+            kb.data.banner = unArrayizeValue(inject.getValue(query, safeCharEncode=True))
+
+        return kb.data.banner
+
+    def getPrivileges(self, *args):
+        warnMsg = "on HSQLDB it is not possible to enumerate the user privileges"
+        logger.warn(warnMsg)
+
+        return {}
+
+    def getHostname(self):
+        warnMsg = "on HSQLDB it is not possible to enumerate the hostname"
+        logger.warn(warnMsg)
